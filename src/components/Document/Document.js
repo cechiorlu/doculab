4370b6b6b5b2d15c94fa747bc62c26879b11c611
Chimzi Chiorlu <edwincheal@gmail.com>
2021-10-29 19:48:34 +0100

    copy link to clipboard on share

--- src/components/Document/Document.js
+++ src/components/Document/Document.js
@@ -7,6 +7,8 @@ import Logo from '../../images/docs-icon.png'
 export default function Document() {
     const [inputValue, setInputValue] = useState('New Document')
     const [docTitle, setDocTitle] = useState(inputValue)
+    const [URL, getURL] = useState('')
+    const [notification, setNotification] = useState(false)
 
 
     // assign web title to ducument title
@@ -15,6 +17,10 @@ export default function Document() {
             title: docTitle,
         })
     })
+
+    useEffect(() => {
+        getURL(window.location.href)
+    }, [])
 
     const handleInputChange = e => {
         setInputValue(e.target.value)
@@ -30,6 +36,20 @@ export default function Document() {
             e.target.blur()
         }
     }
+
+    const urlToClipboard = () => {
+        navigator.clipboard.writeText(URL)
+        setNotification(true)
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setNotification(false)
+        }, 2000)
+        return () => clearTimeout(timer);
+    }, [notification])
+
+
     return (
         <div className="document">
             <div className="document-header">
@@ -41,7 +61,7 @@ export default function Document() {
                     .docx
                 </button>
                 <button className="saved"><i class="fa fa-check-square-o" aria-hidden="true" /> saved</button>
-                <button className="share-btn" onClick={() => { }}>
+                <button className="share-btn" onClick={urlToClipboard}>
                     <i class="fa fa-link" aria-hidden="true" />
                     Share
                 </button>
@@ -52,6 +72,11 @@ export default function Document() {
             </div>
 
             <TextEditor />
+            <div className="copy-notification"
+                style={{ display: notification ? "block" : "none" }}
+            >
+                Link copied to clipboard
+            </div>
         </div>
     )
 }
